bb796e5bfda7903d733fb78b07cffc50f4d11f80
thalisson-sousa <thalissonpop91@gmail.com>
2023-04-21 17:29:57 -0300

    criando validacoes

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,8 +1,8 @@
 export default function ehumCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf)
-
-    
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        campo.setCustomValidity('Esse CPF, Não é Valido!')
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -20,4 +20,40 @@ function validaNumerosRepetidos(cpf) {
     ]
 
     return numeroRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+     let soma = 0;
+     let multiplicador = 10;
+
+     for(let tamanho = 0; tamanho < 9; tamanho ++) {
+        soma += cpf[tamanho] * multiplicador
+        multiplicador--
+     }
+
+     soma = (soma * 10) % 11
+
+     if(soma == 10 || soma == 11) {
+        soma = 0;
+     }
+
+     return soma != cpf[9]
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let tamanho = 0; tamanho < 10; tamanho ++) {
+       soma += cpf[tamanho] * multiplicador
+       multiplicador--
+    }
+
+    soma = (soma * 10) % 11
+
+    if(soma == 10 || soma == 11) {
+       soma = 0;
+    }
+
+    return soma != cpf[10]
 }
